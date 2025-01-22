393b6ba1d1bca50aaeec22761d9b878fd05d4897
dushananuradha <ohda456@gmail.com>
2025-01-23 07:14:57 +1000

    modf_submodules

--- fixtures.ts
+++ fixtures.ts
@@ -1,38 +1,18 @@
-import { test as base } from '@playwright/test';
-import * as fs from 'fs';
-import * as path from 'path';
-import LoginPage from "./pw-ts-automation-submodule-pages/pages/LoginPage";
+import { test as base } from "@playwright/test";
+import * as fs from "fs";
+import * as path from "path";
 
 type TestConfig = {
   baseURL: string;
-  users: {
-    [key: string]: {
-      username: string;
-      password: string;
-    }
-  }
+  username: string;
+  password: string;
 };
 
-export const test = base.extend<{ testConfig: TestConfig, login: (username: string, password: string) => Promise<void>; }>({
-  testConfig: async ({ }, use) => {
-    const configPath = path.join(__dirname,'testConfig.json');
-    const configFile = fs.readFileSync(configPath, 'utf-8');
+export const test = base.extend<{ testConfig: TestConfig }>({
+  testConfig: async ({}, use) => {
+    const configPath = path.join(__dirname, "testConfig.json");
+    const configFile = fs.readFileSync(configPath, "utf-8");
     const config: TestConfig = JSON.parse(configFile);
     await use(config);
-  },
-
-  login: async ({ page }, use) => {
-    const loginPage = new LoginPage(page);
-
-    const login = async (username: string, password: string) => {
-      await loginPage.inputUsername.fill(username);
-      await loginPage.inputPassword.fill(password);
-      await loginPage.btnLogin.click();
-      await page.waitForTimeout(10000);
-    };
-
-    await use(login);
-  },
+  }
 });
-
-export { expect } from '@playwright/test';
