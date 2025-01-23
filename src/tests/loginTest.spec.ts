2bf6fe3c8b92d2225611452d4d5f4b166feaf22f
dushananuradha <ohda456@gmail.com>
2025-01-23 10:20:52 +1000

    modf_loginTest_homePage

--- src/tests/loginTest.spec.ts
+++ src/tests/loginTest.spec.ts
@@ -1,4 +1,5 @@
 import { test } from "../../fixtures";
+import HomePage from "../../pw-ts-automation-submodule-pages/pages/HomePage";
 import LoginPage from "../../pw-ts-automation-submodule-pages/pages/LoginPage";
 import logger from "../logging/LoggerUtil";
 // import { pwTestCredentials } from '../config/credentials';
@@ -10,24 +11,34 @@ test.beforeEach(async ({ page, testConfig }) => {
 test.describe("Test login", () => {
     test("Verify login", async ({ page, testConfig }) => {
         // const { username, password } = pwTestCredentials;
-        const username:any = process.env.PW_USERNAME;
-        const password:any = process.env.PW_PASSWORD;
+        const username: any = process.env.PW_USERNAME;
+        const password: any = process.env.PW_PASSWORD;
 
         if (!username || !password) {
             throw new Error('GitHub secrets for username and password are not set');
         }
         const loginPage = new LoginPage(page);
 
+        try {
+            await loginPage.fillUsername(username);
+            await loginPage.fillPassword(password);
+            await Promise.all([
+                page.waitForEvent('load'),
+                await loginPage.clickLoginButton()
+            ]);
+            const homePage = new HomePage(page);
+            await homePage.verifyHomePageTitleVisibility();
+            logger.info("Test Completed Successfully");
+        } catch (err) {
+            logger.error(`Login test failed: ${err.message}`);
+            throw err
+        }
+
         // await loginPage.fillUsername(testConfig.username);
         // await loginPage.fillPassword(testConfig.password);
 
-        await loginPage.fillUsername(username);
-        console.log('Username length:', process.env.PW_USERNAME?.length || 'Not set');
-        await loginPage.fillPassword(password);
-        console.log('Username length:', process.env.PW_PASSWORD?.length || 'Not set');
-        const homePage = await loginPage.clickLoginButton();
-        await homePage.verifyHomePageTitleVisibility();
-        logger.info("Test Completed");
+
+
 
     });
 });
